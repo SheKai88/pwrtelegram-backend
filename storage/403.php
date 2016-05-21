3ebb9c8d326412df711fef348578cee357a93a7b
Pwrtelegram <daniil.gentili.dg@gmail.com>
2016-05-21 21:56:56 +0200

    Moved download script to main repo and update caddy configuration

--- storage/403.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-?>
-<html>
-<h1>403: Forbidden.</h1>
-<p>I&apos;m sorry, You don't have permission to access this file. Please try again.</p>
-</html>
